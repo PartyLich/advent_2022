39e0499c9c4c656726e8a4341019ecb7dd305def
Arthur E. Jones <arthurjones@austinsoftware.com>
2022-12-01 17:40:58 -0600

    test: add 1-2 test

--- advent_2022/src/day_01/mod.rs
+++ advent_2022/src/day_01/mod.rs
@@ -18,7 +18,7 @@ pub fn one(file_path: &str) -> usize {
         .unwrap();
 }
 
-/// TODO
+/// returns the sum of the largest 3 calories carried
 pub fn two(file_path: &str) -> usize {
     todo!();
 }
@@ -32,6 +32,14 @@ mod test {
         let msg = "should return the largest quantity of calories carried";
         let expected = 24000;
         let actual = one("input/01-t.txt");
+        assert_eq!(actual, expected, "{}", msg);
+    }
+
+    #[test]
+    fn part_two() {
+        let msg = "should return the sum of the largest 3 calories carried";
+        let expected = 45000;
+        let actual = two("input/01-t.txt");
         assert_eq!(actual, expected, "{}", msg);
     }
 }

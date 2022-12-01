18d24163aab4f47d7ed7a495eed8bad8b129fcc1
Arthur E. Jones <arthurjones@austinsoftware.com>
2022-12-01 17:21:01 -0600

    test: add 1-1 test

---
+++ advent_2022/src/day_01/mod.rs
@@ -0,0 +1,25 @@
+//! Solutions to 2022 day 1 problems
+//! --- Day 1: Calorie Counting ---
+
+/// returns the largest quantity of calories carried
+pub fn one(file_path: &str) -> usize {
+    todo!();
+}
+
+/// TODO
+pub fn two(file_path: &str) -> usize {
+    todo!();
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn part_one() {
+        let msg = "should return the largest quantity of calories carried";
+        let expected = 24000;
+        let actual = one("input/01-t.txt");
+        assert_eq!(actual, expected, "{}", msg);
+    }
+}
